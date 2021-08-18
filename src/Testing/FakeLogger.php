3fa17a7edad825c2053119cb7f090322879b2bd9
Kai Sassnowski <ksassnowski@users.noreply.github.com>
2021-08-18 07:38:45 +0200

    Add FakeLogger implementation

---
+++ src/Testing/FakeLogger.php
@@ -0,0 +1,90 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2021 Kai Sassnowski
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @see https://github.com/roach-php/roach
+ */
+
+namespace Sassnowski\Roach\Testing;
+
+use Psr\Log\LoggerInterface;
+
+final class FakeLogger implements LoggerInterface
+{
+    private array $logs = [
+        'emergency' => [],
+        'alert' => [],
+        'critical' => [],
+        'error' => [],
+        'warning' => [],
+        'notice' => [],
+        'info' => [],
+        'debug' => [],
+    ];
+
+    public function emergency($message, array $context = []): void
+    {
+        $this->log('emergency', $message, $context);
+    }
+
+    public function alert($message, array $context = []): void
+    {
+        $this->log('alert', $message, $context);
+    }
+
+    public function critical($message, array $context = []): void
+    {
+        $this->log('critical', $message, $context);
+    }
+
+    public function error($message, array $context = []): void
+    {
+        $this->log('error', $message, $context);
+    }
+
+    public function warning($message, array $context = []): void
+    {
+        $this->log('warning', $message, $context);
+    }
+
+    public function notice($message, array $context = []): void
+    {
+        $this->log('notice', $message, $context);
+    }
+
+    public function info($message, array $context = []): void
+    {
+        $this->log('info', $message, $context);
+    }
+
+    public function debug($message, array $context = []): void
+    {
+        $this->log('debug', $message, $context);
+    }
+
+    public function log($level, $message, array $context = []): void
+    {
+        $this->logs[$level][] = [
+            'message' => $message,
+            'context' => $context,
+        ];
+    }
+
+    public function messageWasLogged(string $level, string $message, ?array $context = null): bool
+    {
+        if (empty($this->logs[$level])) {
+            return false;
+        }
+
+        return !empty(\array_filter($this->logs[$level], static function (array $log) use ($message, $context) {
+            return $log['message'] === $message
+                && (null === $context || $log['context'] === $context);
+        }));
+    }
+}
---
+++ tests/Testing/FakeLoggerTest.php
@@ -0,0 +1,85 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2021 Kai Sassnowski
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @see https://github.com/roach-php/roach
+ */
+
+namespace Sassnowski\Roach\Tests\Testing;
+
+use Generator;
+use PHPUnit\Framework\TestCase;
+use Sassnowski\Roach\Testing\FakeLogger;
+
+/**
+ * @covers \Sassnowski\Roach\Testing\FakeLogger
+ * @group testing
+ *
+ * @internal
+ */
+final class FakeLoggerTest extends TestCase
+{
+    /**
+     * @dataProvider logMessageProvider
+     */
+    public function testCheckIfSpecificMessageWasLoggedAtLevel(string $level, string $message, array $context): void
+    {
+        $logger = new FakeLogger();
+
+        self::assertFalse($logger->messageWasLogged($level, $message));
+
+        $logger->{$level}($message, $context);
+
+        self::assertTrue($logger->messageWasLogged($level, $message));
+    }
+
+    /**
+     * @dataProvider logMessageProvider
+     */
+    public function testCheckIfMessageWasLoggedWithContext(string $level, string $message, array $context): void
+    {
+        $logger = new FakeLogger();
+
+        $logger->{$level}($message, []);
+        self::assertFalse($logger->messageWasLogged($level, $message, $context));
+
+        $logger->{$level}($message, $context);
+        self::assertTrue($logger->messageWasLogged($level, $message, $context));
+    }
+
+    public function logMessageProvider(): Generator
+    {
+        yield from [
+            'debug' => [
+                'debug', '::debug-message::', ['::debug-context::'],
+            ],
+            'info' => [
+                'info', '::info-message::', ['::info-context::'],
+            ],
+            'notice' => [
+                'notice', '::notice-message::', ['::notice-context::'],
+            ],
+            'warning' => [
+                'warning', '::warning-message::', ['::warning-context::'],
+            ],
+            'error' => [
+                'error', '::error-message::', ['::error-context::'],
+            ],
+            'critical' => [
+                'critical', '::critical-message::', ['::critical-context::'],
+            ],
+            'alert' => [
+                'alert', '::alert-message::', ['::alert-context::'],
+            ],
+            'emergency' => [
+                'emergency', '::emergency-message::', ['::emergency-context::'],
+            ],
+        ];
+    }
+}
