aa0eed33df8c5ab8a1a86f5e56f945a8dc58c047
Kai Sassnowski <me@kai-sassnowski.com>
2021-12-23 12:17:53 +0100

    Add test for ArrayLoader

--- tests/Spider/Configuration/ArrayLoaderTest.php
+++ tests/Spider/Configuration/ArrayLoaderTest.php
@@ -1,8 +1,75 @@
 <?php
 
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2021 Kai Sassnowski
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @see https://github.com/roach-php/roach
+ */
+
 namespace RoachPHP\Tests\Spider\Configuration;
 
-class ArrayLoaderTest
-{
+use PHPUnit\Framework\TestCase;
+use RoachPHP\Extensions\LoggerExtension;
+use RoachPHP\Spider\Configuration\ArrayLoader;
+use RoachPHP\Spider\Configuration\Configuration;
 
+/**
+ * @internal
+ */
+final class ArrayLoaderTest extends TestCase
+{
+    public function testLoadDefaultConfiguration(): void
+    {
+        $loader = new ArrayLoader([]);
+
+        $actual = $loader->load();
+
+        $expected = new Configuration([], [], [], [], [], 5, 0);
+        self::assertEquals($expected, $actual);
+    }
+
+    public function testMergePartialOptions(): void
+    {
+        $loader = new ArrayLoader([
+            'startUrls' => ['::start-url::'],
+            'extensions' => [LoggerExtension::class],
+            'concurrency' => 2,
+        ]);
+
+        $actual = $loader->load();
+
+        $expected = new Configuration(['::start-url::'], [], [], [], [LoggerExtension::class], 2, 0);
+        self::assertEquals($expected, $actual);
+    }
+
+    public function testMergeAllOptions(): void
+    {
+        $loader = new ArrayLoader([
+            'startUrls' => ['::start-url::'],
+            'downloaderMiddleware' => ['::downloader-middleware::'],
+            'spiderMiddleware' => ['::spider-middleware::'],
+            'itemProcessors' => ['::item-processor::'],
+            'extensions' => [LoggerExtension::class],
+            'concurrency' => 2,
+            'requestDelay' => 2,
+        ]);
+
+        $actual = $loader->load();
+
+        $expected = new Configuration(
+            ['::start-url::'],
+            ['::downloader-middleware::'],
+            ['::item-processor::'],
+            ['::spider-middleware::'],
+            [LoggerExtension::class],
+            2,
+            2
+        );
+        self::assertEquals($expected, $actual);
+    }
 }
