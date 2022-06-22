7ba1a70e07c148926d5b99080caaafbc4c31be11
Kai Sassnowski <me@kai-sassnowski.com>
2022-06-22 07:36:30 +0200

    Add ConditionalItemProcessor interface and handle it in pipeline

---
+++ src/ItemPipeline/Processors/ConditionalItemProcessor.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2022 Kai Sassnowski
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @see https://github.com/roach-php/roach
+ */
+
+namespace RoachPHP\ItemPipeline\Processors;
+
+use RoachPHP\ItemPipeline\ItemInterface;
+
+interface ConditionalItemProcessor extends ItemProcessorInterface
+{
+    /**
+     * Check if the yielded item should get handled by this item processor.
+     */
+    public function shouldHandle(ItemInterface $item): bool;
+}
